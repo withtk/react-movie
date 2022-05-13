c1730d53ddf4d7c56f83eb0479f28da3ecc655f6
tki <dnf126@gmail.com>
2022-05-13 18:18:39 +0900

    v1 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRoute, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 하이루
-        </a>
-      </header>
-    </div>
+    <BrowserRoute>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRoute> 
   );
 }
 
