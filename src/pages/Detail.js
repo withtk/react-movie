86b228c45e36aeeabba5a23239c0ed4e24e9b474
tki <dnf126@gmail.com>
2022-07-04 18:06:20 +0900

    arranged

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,47 +1,42 @@
-import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
-import {useParams} from 'react-router-dom';
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
+function Detail() {
+  const { id } = useParams();
+  const { movie, setMovie } = useState();
 
-function Detail( ) {
-    const { id } = useParams();
-    const {movie,setMovie} = useState();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json.data.movie);
 
-    const getMovie =async () => {
-        const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
-        console.log(json.data.movie);
+    setMovie(json.data.movie);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
 
-        setMovie(json.data.movie);
-    }
-    useEffect(() => {
-        getMovie();
-    },[]);
-
-    return (
-        <div> 
-            <h1>Detail..!.</h1>
-            movie QQQQQ <div>
-
-            </div>
-            {/* <h1>{title}</h1>
+  return (
+    <div>
+      <h1>Detail..!.</h1>
+      movie QQQQQ <div></div>
+      {/* <h1>{title}</h1>
             <p>{summary}</p>
                 <img src={coverImg} alt={title} />                
             <ul>
                 {genres.map((g) => (<li>{g}</li>))}
             </ul>  */}
-            
-        </div>
-    );
-
-    
+    </div>
+  );
 }
- 
+
 Detail.propTypes = {
-    id:PropTypes.string.isRequired,
-    coverImg : PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    genres:PropTypes.arrayOf(PropTypes.string).isRequired
-}
+  id: PropTypes.string.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 export default Detail;
-
