f335291a5f15ee99b00b994704e29281f8b6799f
terrific <psst126@gmail.com>
2022-07-11 19:19:12 +0900

    notebook first

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,29 +1,29 @@
 import PropTypes from "prop-types";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-
-function Movie({id, coverImg,title,summary,genres}) {
-      
-    return (
-        <div>
-            <h1>{title}</h1>
-            <p>{summary}</p>
-            <Link to={`/movie/${id}`}>
-                <img src={coverImg} alt={title} />                
-            </Link>
-            <ul>
-                {genres.map((g) => (<li>{g}</li>))}
-            </ul>
-        </div>
-    );
+function Movie({ id, coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{summary}</p>
+      <Link to={`/movie/${id}`}>
+        <img src={coverImg} alt={title} />
+      </Link>
+      <ul>
+        {genres.map((g) => (
+          <li>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-    id:PropTypes.string.isRequired,
-    coverImg : PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    genres:PropTypes.arrayOf(PropTypes.string).isRequired
-}
+  id: PropTypes.string.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Movie;
