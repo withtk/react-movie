a2af080ac4bab1cfa2b0e42c08587057d1781dc4
taekyung.kim <orora@ryencatchers.com>
2024-06-15 02:43:40 +0900

    Fix | Refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import 'antd/dist/antd.css';
+import './styles/App.css';
+// import 'antd/dist/antd.css';
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Home from './pages/home/Home';
 import Main from './pages/Main';
-import Menu2 from './menu/menu2'; 
+import Menu2 from './menu/menu2';
 
 const MovieList = lazy(() => import('./pages/movie/MovieList'));
 const Home = lazy(() => import('./pages/home/Home'));
-const TTTest = lazy(() => import('./TTTest'));
+const TTTest = lazy(() => import('./test/TTTest'));
 const Login = lazy(() => import('./pages/login/Login'));
 const TestInput = lazy(() => import('./test/TestInput'));
 
@@ -20,7 +20,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Main />} />
 
-          <Route path='home' element={<Home />} /> 
+          <Route path='home' element={<Home />} />
           <Route path='menu2' element={<Menu2 />} />
           <Route path='TTT' element={<TTTest />} />
           <Route path='login' element={<Login />} />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,26 +1,25 @@
-
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import {useEffect} from 'react';
+import {Link} from 'react-router-dom';
 import './home.css';
 
- function Home() {
-    useEffect(() => {
-        console.log('컴포넌트가 화면에 나타남');
-        return () => {
-          console.log('컴포넌트가 화면에서 사라짐');
-        };
-      }, []);
-    return (
-        <div className="first">
-            <h1>home start</h1>
-            <Link to="movieList" >Movie List로 이동</Link> <br/> <br/>
-            <Link to="main" >Main</Link> <br/> <br/>
-            <Link to="menu2" >menu2</Link><br/> <br/>
-            <Link to="TTT" >TTTest</Link><br/> <br/>
-            <Link to="login" >Login</Link><br/> <br/>
-            <Link to="testInput" >testInput</Link><br/> <br/>
-        </div>
-    )
-}
+export default function Home() {
 
-export default Home;
+  useEffect(() => {
+    console.log('컴포넌트가 화면에 나타남');
+    return () => {
+      console.log('컴포넌트가 화면에서 사라짐');
+    };
+  }, []);
+
+  return (
+    <div className="first">
+      <h1>home start</h1>
+      <Link to="movieList">Movie List로 이동</Link> <br/> <br/>
+      <Link to="main">Main</Link> <br/> <br/>
+      <Link to="menu2">menu2</Link><br/> <br/>
+      <Link to="TTT">TTTest</Link><br/> <br/>
+      <Link to="login">Login</Link><br/> <br/>
+      <Link to="testInput">testInput</Link><br/> <br/>
+    </div>
+  )
+}
